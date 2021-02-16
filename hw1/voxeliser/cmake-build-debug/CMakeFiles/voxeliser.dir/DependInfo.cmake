2c1be8077d203a36320a70166b9cb5480310c950
Michiel de Jong <m.d.dejong-1@student.tudelft.nl>
2021-02-16 12:20:05 +0100

    Initial Commit

---
+++ hw1/voxeliser/cmake-build-debug/CMakeFiles/voxeliser.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/michieldejong/Documents/GEOMATICS/GEO1004/hw1/voxeliser/main.cpp" "/Users/michieldejong/Documents/GEOMATICS/GEO1004/hw1/voxeliser/cmake-build-debug/CMakeFiles/voxeliser.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ hw1/voxeliser/cmake-build-debug/CMakeFiles/voxeliser.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/voxeliser.dir/main.cpp.o"
+  "voxeliser"
+  "voxeliser.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/voxeliser.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
