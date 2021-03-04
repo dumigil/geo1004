a45ef4b9114375adb5e866bd302dc7dfeda6dd1b
Michiel de Jong <m.d.dejong-1@student.tudelft.nl>
2021-03-04 15:28:26 +0100

    Add build

---
+++ hw2/soupchef/cmake-build-debug/CMakeFiles/soupchef.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/soupchef.dir/main.cpp.o"
+  "soupchef"
+  "soupchef.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/soupchef.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
